629469b11927b758f9b9706c7ea8ec4b24c32b03
Maddukuritarun <66254626+Maddukuritarun@users.noreply.github.com>
2021-03-11 18:38:59 +0530

    Add files via upload

---
+++ LabookAssignments/src/com/cg/assign/Lab7/src/com/cg/assign/pl/EmployeeDetails.java
@@ -0,0 +1,13 @@
+package com.cg.assign.pl;
+
+
+import com.cg.assign.service.EmployeeServiceImpl;
+
+public class EmployeeDetails {
+	public static void main(String[] args) {
+		EmployeeServiceImpl Esobj=new EmployeeServiceImpl();
+		Esobj.getDetailsFromUser();
+		
+	}
+	
+}
---
+++ LabookAssignments/src/com/cg/assign/Lab7/src/com/cg/assign/service/EmployeeService.java
@@ -0,0 +1,8 @@
+package com.cg.assign.service;
+
+
+public interface EmployeeService {
+	void getDetailsFromUser();
+	String insuranceScheme(int salary,String designation);
+}
+
